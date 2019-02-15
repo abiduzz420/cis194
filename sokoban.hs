fb0e73f4c4b70c456e53e19d4910cfca56e4c201
abiduzz420 <abiduzair420@gmail.com>
2019-02-16 03:37:48 +0530

    list of mazes added and list helper functions too

--- sokoban.hs
+++ sokoban.hs
@@ -13,6 +13,31 @@ combine :: List Picture -> Picture
 combine Empty = blank
 combine (Entry p ps) = p & combine ps
 
+elemList :: Eq a => List a -> Bool
+elemList (Entry x xs) = contains xs x
+
+appendList :: List a -> List a -> List a
+appendList a Empty = a
+appendList a (Entry b bRest) = appendList nA bRest
+  where
+    nA = Entry b a
+
+listLength :: List a -> Integer
+listLength Empty = 0
+listLength (Entry x xs) = 1 + listLength xs 
+
+filterList :: (a -> Bool) -> List a -> List a
+filterList _ Empty = Empty
+filterList p (Entry x xs)
+  | p x = Entry x (filterList p xs)
+  | otherwise = filterList p xs
+
+nths :: List a -> Integer -> a
+nths (Entry x xs) n
+  | n > listLength (Entry x xs) = error "list too short"
+  | n == 1 = x
+  | otherwise = nths xs (n-1)
+
 -- Coordinates
 
 data Coord = C Integer Integer
@@ -35,18 +60,12 @@ data Tile = Wall | Ground | Storage | Box | Blank deriving Eq
 
 maze :: Coord -> Tile
 maze (C x y)
-  | abs x >  4 || abs y >  4           = Blank
-  | abs x == 4 || abs y == 4           = Wall
-  | x ==     1 && y <      0           = Wall
-  | x ==    -3 && y ==    -2           = Wall
-  | x <=     1 && x >     -2 && y == 0 = Wall
-  | x >     -3 && x <      3 && y == 2 = Wall
-  | x ==     3 && y >      1           = Storage
-  | y ==    -2 && x <      0           = Box
-  | y ==    -2 && x ==     2           = Box
-  | y ==    0  && x ==     3           = Box
-  | y == -1    && x > 1      && x < 4  = Storage
-  | otherwise                          = Ground
+  | abs x > 4  || abs y > 4  = Blank
+  | abs x == 4 || abs y == 4 = Wall
+  | x ==  2 && y <= 1        = Wall
+  | x ==  3 && y <= 0        = Storage
+  | x >= -2 && y == 0        = Box
+  | otherwise                = Ground
   
 boxes :: List Coord -> Picture
 boxes cs = combine (mapList (\c -> atCoord c (drawTile Box)) cs)
@@ -56,7 +75,7 @@ noBoxMaze c = case (maze c) of
   Box -> Ground
   t -> t
  
-contains :: List Coord -> Coord -> Bool
+contains :: Eq a => List a -> a -> Bool
 contains Empty _ = False
 contains (Entry c cs) coord = c == coord || contains cs coord
 
@@ -69,13 +88,6 @@ mazeWithBoxes list coord
 -- The state
 
 data State = S Coord Direction (List Coord) deriving Eq
-
-appendList :: List a -> List a -> List a
-appendList a Empty = a
-appendList a (Entry b bRest) = appendList nA bRest
-  where
-    nA = Entry b a
-
 
 getBox :: Coord -> List Coord
 getBox c
@@ -306,3 +318,248 @@ sokoban = Interaction initState handleTime handleEvent drawState
 
 main :: IO ()
 main = runInteraction (resetable (undoInteraction (withStartScreen sokoban)))
+
+-- List of all mazes
+
+data Maze = Maze Coord (Coord -> Tile) 
+
+mazes :: List Maze
+mazes =
+  Entry (Maze (C 1 1)       maze9) $
+  Entry (Maze (C 0 0)       maze8) $
+  Entry (Maze (C (-3) 3)    maze7) $
+  Entry (Maze (C (-2) 4)    maze6) $
+  Entry (Maze (C 0 1)       maze5) $
+  Entry (Maze (C 1 (-3))    maze4) $
+  Entry (Maze (C (-4) 3)    maze3) $
+  Entry (Maze (C 0 1)       maze1) $
+  Empty
+  
+extraMazes :: List Maze
+extraMazes =
+  Entry (Maze (C 1 (-3))    maze4') $
+  Entry (Maze (C 1 (-3))    maze4'') $
+  Entry (Maze (C 1 1)       maze9') $
+  mazes
+
+maze1 :: Coord -> Tile 
+maze1 (C x y)
+  | abs x > 4  || abs y > 4  = Blank
+  | abs x == 4 || abs y == 4 = Wall
+  | x ==  2 && y <= 0        = Wall
+  | x ==  3 && y <= 0        = Storage
+  | x >= -2 && y == 0        = Box
+  | otherwise                = Ground
+
+maze3 :: Coord -> Tile
+maze3 (C (-5) (-5)) = Wall
+maze3 (C (-5) (-4)) = Wall
+maze3 (C (-5) (-3)) = Wall
+maze3 (C (-5) (-2)) = Wall
+maze3 (C (-5) (-1)) = Wall
+maze3 (C (-5)   0 ) = Wall
+maze3 (C (-5)   1 ) = Wall
+maze3 (C (-5)   2 ) = Wall
+maze3 (C (-5)   3 ) = Wall
+maze3 (C (-5)   4 ) = Wall
+
+maze3 (C (-4) (-5)) = Wall
+maze3 (C (-4) (-4)) = Ground
+maze3 (C (-4) (-3)) = Ground
+maze3 (C (-4) (-2)) = Ground
+maze3 (C (-4) (-1)) = Ground
+maze3 (C (-4)   0 ) = Ground
+maze3 (C (-4)   1 ) = Ground
+maze3 (C (-4)   2 ) = Ground
+maze3 (C (-4)   3 ) = Ground
+maze3 (C (-4)   4 ) = Wall
+
+maze3 (C (-3) (-5)) = Wall
+maze3 (C (-3) (-4)) = Ground
+maze3 (C (-3) (-3)) = Wall
+maze3 (C (-3) (-2)) = Wall
+maze3 (C (-3) (-1)) = Wall
+maze3 (C (-3)   0 ) = Wall
+maze3 (C (-3)   1 ) = Ground
+maze3 (C (-3)   2 ) = Wall
+maze3 (C (-3)   3 ) = Ground
+maze3 (C (-3)   4 ) = Wall
+maze3 (C (-3)   5 ) = Wall
+
+maze3 (C (-2) (-5)) = Wall
+maze3 (C (-2) (-4)) = Box
+maze3 (C (-2) (-3)) = Ground
+maze3 (C (-2) (-2)) = Ground
+maze3 (C (-2) (-1)) = Ground
+maze3 (C (-2)   0 ) = Wall
+maze3 (C (-2)   1 ) = Ground
+maze3 (C (-2)   2 ) = Box
+maze3 (C (-2)   3 ) = Box
+maze3 (C (-2)   4 ) = Ground
+maze3 (C (-2)   5 ) = Wall
+
+maze3 (C (-1) (-6)) = Wall
+maze3 (C (-1) (-5)) = Wall
+maze3 (C (-1) (-4)) = Ground
+maze3 (C (-1) (-3)) = Ground
+maze3 (C (-1) (-2)) = Ground
+maze3 (C (-1) (-1)) = Ground
+maze3 (C (-1)   0 ) = Wall
+maze3 (C (-1)   1 ) = Ground
+maze3 (C (-1)   2 ) = Ground
+maze3 (C (-1)   3 ) = Box
+maze3 (C (-1)   4 ) = Ground
+maze3 (C (-1)   5 ) = Wall
+maze3 (C (-1)   6 ) = Wall
+
+maze3 (C   0  (-6)) = Wall
+maze3 (C   0  (-5)) = Ground
+maze3 (C   0  (-4)) = Ground
+maze3 (C   0  (-3)) = Ground
+maze3 (C   0  (-2)) = Ground
+maze3 (C   0  (-1)) = Ground
+maze3 (C   0    0 ) = Wall
+maze3 (C   0    1 ) = Wall
+maze3 (C   0    2 ) = Wall
+maze3 (C   0    3 ) = Wall
+maze3 (C   0    4 ) = Ground
+maze3 (C   0    5 ) = Ground
+maze3 (C   0    6 ) = Wall
+
+maze3 (C   1  (-6)) = Wall
+maze3 (C   1  (-5)) = Ground
+maze3 (C   1  (-4)) = Ground
+maze3 (C   1  (-3)) = Ground
+maze3 (C   1  (-2)) = Ground
+maze3 (C   1  (-1)) = Ground
+maze3 (C   1    0 ) = Wall
+maze3 (C   1    1 ) = Storage
+maze3 (C   1    2 ) = Storage
+maze3 (C   1    3 ) = Storage
+maze3 (C   1    4 ) = Ground
+maze3 (C   1    5 ) = Ground
+maze3 (C   1    6 ) = Wall
+
+maze3 (C   2  (-6)) = Wall
+maze3 (C   2  (-5)) = Wall
+maze3 (C   2  (-4)) = Ground
+maze3 (C   2  (-3)) = Ground
+maze3 (C   2  (-2)) = Ground
+maze3 (C   2  (-1)) = Ground
+maze3 (C   2    0 ) = Wall
+maze3 (C   2    1 ) = Wall
+maze3 (C   2    2 ) = Wall
+maze3 (C   2    3 ) = Wall
+maze3 (C   2    4 ) = Wall
+maze3 (C   2    5 ) = Wall
+maze3 (C   2    6 ) = Wall
+
+maze3 (C   3  (-5)) = Wall
+maze3 (C   3  (-4)) = Ground
+maze3 (C   3  (-3)) = Ground
+maze3 (C   3  (-2)) = Storage
+maze3 (C   3  (-1)) = Ground
+maze3 (C   3    0 ) = Wall
+
+maze3 (C   4  (-5)) = Wall
+maze3 (C   4  (-4)) = Wall
+maze3 (C   4  (-3)) = Wall
+maze3 (C   4  (-2)) = Wall
+maze3 (C   4  (-1)) = Wall
+maze3 (C   4    0 ) = Wall
+
+maze3 _ = Blank
+
+maze4 :: Coord -> Tile
+maze4 (C x y)
+  | abs x > 4  || abs y > 4      = Blank
+  | abs x == 4 || abs y == 4     = Wall
+  | x ==  2 && y <   0           = Wall
+  | x >= -1 && y ==  1 && x <= 2 = Wall
+  | x == -3 && y ==  1           = Wall
+  | x ==  0 && y ==  3           = Wall
+  | x ==  0 && y ==  0           = Wall
+  | x ==  3 && y == -3           = Storage
+  | x ==  1 && y ==  2           = Storage
+  | x == -3 && y ==  2           = Storage
+  | x ==  1 && y == -1           = Storage
+  | x == -2 && y ==  1           = Box
+  | x ==  2 && y ==  2           = Box
+  | x <=  1 && y == -2 && x >= 0 = Box
+  | otherwise                    = Ground
+
+maze5 :: Coord -> Tile 
+maze5 (C x y)
+  | abs x >  4 || abs y >  4           = Blank
+  | abs x == 4 || abs y == 4           = Wall
+  | x ==     1 && y <      0           = Wall
+  | x ==    -3 && y ==    -2           = Wall
+  | x <=     1 && x >     -2 && y == 0 = Wall
+  | x >     -3 && x <      3 && y == 2 = Wall
+  | x ==     3 && y >      1           = Storage
+  | y ==    -2 && x <      0           = Box
+  | y ==    -2 && x ==     2           = Box
+  | y ==    0  && x ==     3           = Box
+  | y == -1    && x > 1      && x < 4  = Storage
+  | otherwise                          = Ground
+
+maze6 :: Coord -> Tile 
+maze6 (C x y)
+  | abs x > 3  || abs y > 5                 = Blank
+  | abs x == 3 || (abs y == 5 && abs x < 4) = Wall
+  | x == 0 && abs y < 4                     = Storage
+  | x == -1 && (y == 0 || abs y == 2)       = Box
+  | x == 1 && (abs y == 1 || abs y == 3)    = Box
+  | x == (-2) &&  y == 1                    = Wall
+  | otherwise                               = Ground
+
+maze7 :: Coord -> Tile
+maze7 (C x y)
+  | abs x > 4  || abs y > 4   = Blank
+  | abs x == 4 || abs y == 4  = Wall
+  | not (x == 2)  && y == 2   = Wall
+  | not (x == -2)  && y == -1 = Wall
+  | x ==  3 && y == -3        = Storage
+  | x == 2 && y == 2          = Box
+  | otherwise                 = Ground
+  
+maze8 :: Coord -> Tile
+maze8 (C x y)
+  | abs x > 10 || abs y > 10    = Blank
+  | x == 0 && y == 0            = Ground
+  | abs x == 9 && abs y == 9    = Wall
+  | abs x == 10 || abs y == 10  = Wall
+  | x == y                      = Storage
+  | abs x == abs y              = Box
+  | x < 0 && x > (-9) && y == 0 = Box
+  | x > 0 && x < 9 && y == 0    = Storage
+  | otherwise                   = Ground
+
+maze9 :: Coord -> Tile 
+maze9 (C x y)
+  | abs x > 4  || abs y > 4                  = Blank
+  | abs x == 4 || abs y == 4 || x == -3      = Wall
+  | x == -2 && (y == 3 || y == 0)            = Wall
+  | x == -1 &&  y == -1                      = Wall
+  | x == -0 &&  y == 1                       = Wall
+  | x ==  3 &&  y == 0                       = Wall
+  | x <   0 && (y == 2 || y == -3)           = Storage
+  | x == -1 &&  y == 1                       = Storage
+  | x ==  0 && (y == 2 || y == 0 || y == -1) = Box
+  | x ==  1 &&  y == -2                      = Box
+  | x ==  2 &&  y == -3                      = Box
+  | otherwise                                = Ground
+
+maze4'' :: Coord -> Tile
+maze4'' (C 1 (-3)) = Box
+maze4'' c = maze4 c
+
+maze4' :: Coord -> Tile
+maze4' (C 0 1) = Blank
+maze4' c = maze4 c
+
+maze9' :: Coord -> Tile
+maze9' (C 3 0) = Box
+maze9' (C 4 0) = Box
+maze9'  c      = maze9 c
+
