b001cbed3fdaee6f55c2f81bd4d0c33df8218885
abiduzz420 <abiduzair420@gmail.com>
2019-03-04 13:52:27 +0530

    linting fixed

--- real-world-haskell.hs
+++ real-world-haskell.hs
@@ -2,14 +2,12 @@
   Real World Haskell Exercise CIS194
   https://www.seas.upenn.edu/~cis194/fall16/hw/05-real-world-haskell.html
 -}
-
 import Data.Char
 import Data.Function
 import Data.List
 import Data.Ord
 
 -- Exercise 1
-
 -- alternate way of expressing lambda
 halveEvens :: [Integer] -> [Integer]
 halveEvens = map (\x -> x `div` 2) . filter even
@@ -53,7 +51,8 @@ addPolynomials list = (map sum . transpose) list
 
 sumNumbers :: String -> Integer
 sumNumbers "" = 0
-sumNumbers str = toInteger $ sum $ map stringToInt $ words $ map addSpaceAtChar str
+sumNumbers str =
+  toInteger $ sum $ map stringToInt $ words $ map addSpaceAtChar str
   where
     addSpaceAtChar :: Char -> Char
     addSpaceAtChar c
@@ -64,34 +63,34 @@ sumNumbers str = toInteger $ sum $ map stringToInt $ words $ map addSpaceAtChar 
     stringToInt str = digitToInt (last str) + 10 * stringToInt (init str)
 
 -- Exercise 2
-
 contains :: String -> [String] -> Bool
 contains _ [] = False
-contains str (x:xs) = str == x || contains str xs 
+contains str (x:xs) = str == x || contains str xs
 
 unique :: [String] -> [String] -> [String]
 unique uniqueList list
   | list == [] = uniqueList
-  | not (contains x uniqueList) = unique (x:uniqueList) xs
-	| otherwise = unique uniqueList xs
-	where
-		(x:xs) = list
+  | not (contains x uniqueList) = unique (x : uniqueList) xs
+  | otherwise = unique uniqueList xs
+  where
+    (x:xs) = list
 
 wordCount :: String -> String
-wordCount str = unlines [
-                    "Number of lines: " ++ (show numLines),
-                    "Number of emptylines: " ++ (show emptyLines),
-                    "Number of words: " ++ (show numWords),
-                    "Number of unique words: " ++ (show uniqueWords),
-                    "Number of words followed by themselves: " ++ (show sameWords),
-                    "Length of the longest line: " ++ (show longestLine)
-                  ]
-								where
-									allLines = lines str
-									numLines = length allLines
-									emptyLines = (length . filter (== "")) allLines
-									allWords = words str
-									numWords = length allWords
-									uniqueWords = length $ unique [] allWords
-									sameWords = (length . filter (\(x,y) -> x == y) . adjacents) allWords
-									longestLine = longestText allLines
+wordCount str =
+  unlines
+    [ "Number of lines: " ++ (show numLines)
+    , "Number of emptylines: " ++ (show emptyLines)
+    , "Number of words: " ++ (show numWords)
+    , "Number of unique words: " ++ (show uniqueWords)
+    , "Number of words followed by themselves: " ++ (show sameWords)
+    , "Length of the longest line: " ++ (show longestLine)
+    ]
+  where
+    allLines = lines str
+    numLines = length allLines
+    emptyLines = (length . filter (== "")) allLines
+    allWords = words str
+    numWords = length allWords
+    uniqueWords = length $ unique [] allWords
+    sameWords = (length . filter (\(x, y) -> x == y) . adjacents) allWords
+    longestLine = longestText allLines
